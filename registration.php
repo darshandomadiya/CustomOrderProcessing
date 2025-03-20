3c4566482b23512a152343cfb86d952207f61a4f
darshandomadiya <42324753+darshandomadiya@users.noreply.github.com>
2025-03-20 14:13:43 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Vendor_CustomOrderProcessing',
+    'DarshanDomadiya_CustomOrderProcessing',
     __DIR__
 );
